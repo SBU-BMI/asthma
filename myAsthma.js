1c53587e3f297001e0c8310f75803c2bb70f51af
Jonas Almeida <jalmeida@mathbiol.org>
2016-05-02 12:57:38 -0400

    show plan

--- myAsthma.js
+++ myAsthma.js
@@ -5,11 +5,11 @@ console.log('loaded myAsthma.js :-)');
     var h ='</div>'
     h += '<h3 style="color:maroon">Welcome to your action plan</h3>'
     h += '<div style="color:blue" id="msg"> Start by loading an Action Plan:</div>'
-    h += '<input type="file" id="inputFile">'
-    h += '<hr><img id="myAsthmaPic">'
+    h += '<div id="loadFileDiv"><input type="file" id="inputFile"></div>'
+    h += '<hr><div id="myAsthmaPicDiv" hidden><img id="myAsthmaPic" class="zoomTarget"></div>'
     h += '</div>'
-    //sbmApps.render(h)
-    document.body.innerHTML=h
+    sbmApps.render(h)
+    //document.body.innerHTML=h
     var reader = new FileReader()
     reader.onload=function(f){
         localStorage.setItem('myAsthmaPic',f.target.result)
@@ -20,12 +20,19 @@ console.log('loaded myAsthma.js :-)');
     var loadImg=function(){
         var imgSrc = localStorage.myAsthmaPic
         if(imgSrc){
-            myAsthmaPic.src=localStorage.myAsthmaPic
-            msg.innerHTML='you can load a new action plan if you want <span id="adPlan" style="color:red">+</span>'
-            adPlan.onclick=function(){
-                inputFile.hidden=false
+            myAsthmaPic.src=imgSrc
+            msg.innerHTML='You have a <a href="#" id="gotoPlanOnFile">plan onfile</a>, you can also load a new action plan if you prefer <span id="addPlan" style="color:green">(+)</span>'
+            addPlan.onclick=function(){
+                if(loadFileDiv.hidden){
+                    loadFileDiv.hidden=false
+                    addPlan.textContent="(-)"
+                }else{
+                    loadFileDiv.hidden=true
+                    addPlan.textContent="(+)"
+                }
+                
             }
-            inputFile.hidden=true
+            loadFileDiv.hidden=true
         }
     }
     inputFile.onchange=function(evt){
@@ -34,7 +41,34 @@ console.log('loaded myAsthma.js :-)');
     }
     loadImg()
 
+    gotoPlanOnFile.onclick=function(tg){
+        console.log('go to asthma action plan on file')
+        myAsthmaPicDiv.hidden=false
+        myAsthmaPic.style.width="100%"
+
+        4
+    }
+
     4
+
+    // dependencies
+
+    /*
+    $.getScript('jquery.zoomooz.min.js',function(){
+        //ez = $('.easyzoom').easyZoom();
+        //api = ez.data('easyZoom')
+    });console.log('loaded zoomooz plugin')
+    */
+
+
+
+
+    /*
+    $.getScript('easyzoom.js',function(){
+        ez = $('.easyzoom').easyZoom();
+        api = ez.data('easyZoom')
+    });console.log('loaded easyzoom plugin')
+    */
 
 
 })()
